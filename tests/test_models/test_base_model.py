e4745754dc5081b07de8b3a446956df75eeae67f
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-03-09 11:37:43 +0200

    test basemodel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Unittesting BaseModel"""
+from models.base_model import BaseModel
+import datetime
+import unittest
+
+
+class TestBaseModel(unittest.TestCase):
+
+    # BaseModel instance is created successfully with default values
+    def test_default_values(self):
+        model = BaseModel()
+        self.assertIsNotNone(model.id)
+        self.assertIsNotNone(model.created_at)
+        self.assertIsNotNone(model.updated_at)
+        self.assertEqual(model.id, model.__dict__["id"])
+        self.assertEqual(model.created_at, model.__dict__["created_at"])
+        self.assertEqual(model.updated_at, model.__dict__["updated_at"])
